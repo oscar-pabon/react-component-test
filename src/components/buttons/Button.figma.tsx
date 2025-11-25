f406fa91a629538257d367d4c89731a3c2f9b866
oskr-pab-n <oxconfessed22@gmail.com>
2025-11-25 10:56:09 -0500

    Publish Button component Code Connect to Figma

--- src/components/buttons/Button.figma.tsx
+++ src/components/buttons/Button.figma.tsx
@@ -5,36 +5,13 @@ figma.connect(
   Button,
   'https://www.figma.com/design/YSsmFp0wm28RUpu5l01XKp/Phoenix-System-Tokens?node-id=1124-7123&m=dev',
   {
-    example: ({ label, variant, disabled, loading, icon, className }) => (
+    example: () => (
       <Button
-        label={label}
-        variant={variant}
-        disabled={disabled}
-        loading={loading}
-        icon={icon}
+        label="Button"
+        variant="primary"
         onClick={() => {}}
-        className={className}
       />
     ),
-    props: {
-      label: figma.string('Label'),
-      variant: figma.enum('Variant', {
-        primary: 'primary',
-        secondary: 'secondary',
-        tertiary: 'tertiary',
-        ghost: 'ghost',
-        destructive: 'destructive',
-      }),
-      disabled: figma.boolean('Disabled'),
-      loading: figma.boolean('Loading'),
-      icon: figma.node('Icon'),
-      rounded: figma.boolean('Rounded'),
-      className: figma.string('Class Name'),
-      type: figma.enum('Type', {
-        button: 'button',
-        submit: 'submit',
-        reset: 'reset',
-      }),
-    },
   }
 );
+
