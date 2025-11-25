9b05e5946b5bcca8317ec4f005dd8c8275902589
oskr-pab-n <oxconfessed22@gmail.com>
2025-11-25 11:16:40 -0500

    Update Button Code Connect mapping with correct properties

--- src/components/buttons/Button.figma.tsx
+++ src/components/buttons/Button.figma.tsx
@@ -1,17 +1,38 @@
-import { figma } from '@figma/code-connect';
-import Button from './Button';
+import React from "react"
+import Button from "./Button"
+import figma from "@figma/code-connect"
+
+/**
+ * Code Connect mapping for the Button component from Phoenix System Tokens
+ */
 
 figma.connect(
   Button,
-  'https://www.figma.com/design/YSsmFp0wm28RUpu5l01XKp/Phoenix-System-Tokens?node-id=1124-7123&m=dev',
+  "https://www.figma.com/design/YSsmFp0wm28RUpu5l01XKp/Phoenix-System-Tokens?node-id=1124-7123&m=dev",
   {
-    example: () => (
+    props: {
+      variant: figma.enum("Variant", {
+        "Primary": "primary",
+        "Secondary": "secondary",
+        "Tertiary": "tertiary",
+        "Ghost": "ghost",
+        "Destructive": "destructive",
+      }),
+      size: figma.enum("Size", {
+        "Small": "small",
+        "Default": "default",
+        "Large": "large",
+      }),
+      disabled: figma.boolean("Disabled"),
+    },
+    example: (props) => (
       <Button
         label="Button"
-        variant="primary"
+        variant={props.variant || "primary"}
+        size={props.size || "default"}
+        disabled={props.disabled || false}
         onClick={() => {}}
       />
     ),
-  }
-);
-
+  },
+)
